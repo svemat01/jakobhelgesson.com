9699f37bf28ec33562e2bd3f9c41ee030e6c0cb5
Svemat01 <jakob.helgesson@gmail.com>
2022-03-28 00:07:10 +0200

    Add secret page

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    let clazz: string = "";
+    export { clazz as class };
+</script>
+
+<section class={clazz}>
+    <slot />
+</section>
+
+<style lang="scss">
+    section {
+        background-color: $gray-color;
+        flex-grow: 1;
+        border-radius: 1.5rem;
+        padding: 2rem;
+        max-width: 60%;
+
+        :global(h1) {
+            color: $accent-color;
+            font-size: 2.5rem;
+            margin-bottom: 1rem;
+        }
+
+        :global(p) {
+            font-size: 1.8rem;
+            margin-bottom: 2rem;
+            display: flex;
+            gap: 1rem;
+            align-items: center;
+        }
+
+        @media screen and (max-width: 1100px) {
+            max-width: 100%;
+        }
+    }
+</style>
---
+++ src/routes/ronne.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import Container from '$components/Container.svelte';
+    import SmallerContainer from '$components/SmallerContainer.svelte';
+
+    import BackgroundShapes from '$components/BackgroundShapes.svelte';
+    import Title from '$components/Title.svelte';
+    import Card from '$components/Card.svelte';
+</script>
+
+<BackgroundShapes />
+
+<SmallerContainer>
+    <Title preTitle="Welcome to the secret page for " title="Rönnegymnasiet" subTitle="" />
+</SmallerContainer>
+
+<Container>
+    <div class="mainContainer">
+        <Card class="bunet">
+            <h1>BUNet-PSK</h1>
+            <p>S0xrYTmIrInBEZCvxKCzspwY0nlJAPAv1ffg6aKF</p>
+        </Card>
+    </div>
+</Container>
+
+<style lang="scss">
+    .mainContainer {
+        width: calc(100% - 25rem);
+        padding-left: 3rem;
+        margin-top: 3rem;
+
+        @media screen and (max-width: 1100px) {
+            width: 100%;
+            padding-left: 0;
+        }
+    }
+
+    /* :global(.bunet) {
+        background-color: aqua !important; 
+    } */
+</style>

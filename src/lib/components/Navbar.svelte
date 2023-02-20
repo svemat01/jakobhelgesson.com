e3325af87a109cb2f878b04fe1487c01899fdc38
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-02-20 23:16:23 +0000

    Add Navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import labsIcon from '../assets/labs-icon.svg';
+</script>
+
+<nav>
+	<div class="container">
+		<a href="/" class="logo">
+			<img src={labsIcon} alt="logo" />
+			<span class="title">Helgesson <span class="thin">Labs</span></span>
+		</a>
+		<div class="links">
+			<a href="/about">About</a>
+			<a href="/contact">Contact</a>
+		</div>
+	</div>
+</nav>
+
+<style lang="scss">
+	nav {
+		background-color: $dark-gray;
+        width: 100%;
+
+        position: sticky;
+        top: 0;
+	}
+
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+
+		height: 58px;
+		max-width: 1200px;
+		width: 100%;
+		margin: 0 auto;
+	}
+
+	.logo {
+		display: flex;
+		align-items: center;
+		gap: 1.4rem;
+
+		img {
+			height: 40px;
+		}
+
+		.title {
+			font-size: 2.4rem;
+			font-weight: 400;
+			color: $white;
+
+            .thin {
+                font-weight: 200;
+            }
+		}
+	}
+
+    .links {
+        display: flex;
+        gap: 2.4rem;
+
+        a {
+            font-size: 2rem;
+            font-weight: 400;
+            color: $white;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import '../app.scss';
 	import '@fontsource/inter'
+	import Navbar from '../lib/components/Navbar.svelte';
 </script>
+
+<Navbar />
 
 <slot />

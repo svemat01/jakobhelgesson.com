5ed413487b4a3fe8535ad157d9bfe34a9fddcbc6
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-02-20 23:17:31 +0000

    Add basic footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import labsIcon from '../assets/labs-icon.svg';
+</script>
+
+<footer>
+        <div class="container">
+            <div class="logo">
+                <img src={labsIcon} alt="Helgesson Labs Logo">
+                <span class="title">Helgesson <span class="thin">Labs</span></span>
+            </div>
+            <!-- <div class="links">
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div> -->
+        </div>
+</footer>
+
+<style lang="scss">
+    footer {
+        background-color: $dark-gray;
+        width: 100%;
+        height: 13rem;
+
+        margin-top: auto;
+
+        padding: 2rem 0;
+    }
+
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr;
+
+        max-width: 1200px;
+        width: 100%;
+        
+        margin: 0 auto;
+    }
+
+    .logo {
+
+        display: flex;
+        align-items: center;
+        gap: 1.4rem;
+
+		.title {
+			font-size: 2.4rem;
+			font-weight: 400;
+			color: $white;
+
+            .thin {
+                font-weight: 200;
+            }
+		}
+
+        img {
+            height: 40px;
+        }
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,11 @@
 	import '../app.scss';
 	import '@fontsource/inter'
 	import Navbar from '../lib/components/Navbar.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 </script>
 
 <Navbar />
 
 <slot />
+
+<Footer />

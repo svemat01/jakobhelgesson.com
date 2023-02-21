1d47e5e09908618785ce763224d0e204090d5959
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-02-21 22:00:27 +0000

    Responsive Navbar

---
+++ src/lib/components/MenuButton.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	export let handleClick: () => void;
+	export let active: boolean;
+</script>
+
+<div on:click={handleClick} on:keydown={handleClick} class="mobile-icon" class:active>
+	<span />
+	<span />
+	<span />
+</div>
+
+<style lang="scss">
+	.mobile-icon {
+		position: relative;
+		width: 30px;
+		height: 30px;
+
+		margin-left: auto;
+
+		@media screen and (min-width: 900px) {
+			display: none;
+		}
+	}
+
+	span {
+		position: absolute;
+		width: 30px;
+		height: 3px;
+		background-color: $white;
+		border-radius: 2px;
+
+		transition: all 0.3s ease;
+
+		&:nth-child(1) {
+			top: 0;
+		}
+
+		&:nth-child(2) {
+			top: 50%;
+			transform: translateY(-50%);
+		}
+
+		&:nth-child(3) {
+			bottom: 0;
+		}
+	}
+
+	.active {
+		span {
+			background-color: $green;
+
+			&:nth-child(1) {
+				top: 50%;
+				transform: translateY(-50%) rotate(45deg);
+			}
+
+			&:nth-child(2) {
+				opacity: 0;
+			}
+
+			&:nth-child(3) {
+				bottom: 50%;
+				transform: translateY(50%) rotate(-45deg);
+			}
+		}
+	}
+</style>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,29 +1,79 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+
 	import labsIcon from '../assets/labs-icon.svg';
+	import MenuButton from './MenuButton.svelte';
+
+	// Show mobile icon and display menu
+	let showMobileMenu = false;
+
+	// List of navigation items
+	const navItems = [
+		{
+			label: 'Home',
+			href: '/'
+		},
+		{
+			label: 'About',
+			href: '/about'
+		},
+		{
+			label: 'Projects',
+			href: '/projects'
+		},
+		{
+			label: 'Contact',
+			href: '/contact'
+		}
+	];
+
+	// Mobile menu click event handler
+	const handleMobileIconClick = () => (showMobileMenu = !showMobileMenu);
+
+	// Media match query handler
+	const mediaQueryHandler = (e: MediaQueryListEvent) => {
+		// Reset mobile state
+		if (!e.matches) {
+			showMobileMenu = false;
+		}
+	};
+
+	// Attach media query listener on mount hook
+	onMount(() => {
+		const mediaListener = window.matchMedia('(max-width: 767px)');
+
+		mediaListener.addEventListener('change', mediaQueryHandler);
+	});
 </script>
 
 <nav>
 	<div class="container">
-		<a href="/" class="logo">
-			<img src={labsIcon} alt="logo" />
-			<span class="title">Helgesson <span class="thin">Labs</span></span>
-		</a>
-		<div class="links">
-			<a href="/about">About</a>
-			<a href="/contact">Contact</a>
+		<div class="main">
+			<a href="/" class="brand">
+				<img src={labsIcon} alt="logo" />
+				<span class="title">Helgesson <span class="thin">Labs</span></span>
+			</a>
+			<MenuButton active={showMobileMenu} handleClick={handleMobileIconClick} />
 		</div>
+		<ul class="navbar-list" class:mobile={showMobileMenu}>
+			{#each navItems as item}
+				<li>
+					<a href={item.href}>{item.label}</a>
+				</li>
+			{/each}
+		</ul>
 	</div>
 </nav>
 
 <style lang="scss">
 	nav {
 		background-color: $dark-gray;
-        width: 100%;
-        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+		width: 100%;
+		box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
 
-        position: sticky;
-        top: 0;
-        z-index: 100;
+		position: sticky;
+		top: 0;
+		z-index: 100;
 	}
 
 	.container {
@@ -31,13 +81,18 @@
 		justify-content: space-between;
 		align-items: center;
 
-		height: 58px;
+		/* height: 64px; */
+		min-height: 64px;
 		max-width: 1200px;
-		width: 100%;
+		width: calc(100% - 4rem);
 		margin: 0 auto;
+
+		.main {
+			display: contents;
+		}
 	}
 
-	.logo {
+	.brand {
 		display: flex;
 		align-items: center;
 		gap: 1.4rem;
@@ -51,20 +106,57 @@
 			font-weight: 400;
 			color: $white;
 
-            .thin {
-                font-weight: 200;
-            }
+			.thin {
+				font-weight: 200;
+			}
 		}
 	}
 
-    .links {
-        display: flex;
-        gap: 2.4rem;
+	.navbar-list {
+		display: flex;
+		gap: 2.4rem;
 
-        a {
-            font-size: 2rem;
-            font-weight: 400;
-            color: $white;
-        }
-    }
+		a {
+			font-size: 2rem;
+			font-weight: 400;
+			color: $white;
+		}
+	}
+
+	@media screen and (max-width: 900px) {
+		.container {
+			flex-direction: column;
+			align-items: center;
+
+			.main {
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				width: 100%;
+
+				padding-top: 1.2rem;
+				padding-bottom: 0.8rem;
+			}
+		}
+
+		.brand {
+			margin-left: auto;
+		}
+
+		.navbar-list {
+			display: none;
+			flex-direction: column;
+			align-items: center;
+			gap: 1.6rem;
+
+			margin-top: 1rem;
+			margin-bottom: 1.6rem;
+
+			width: 100%;
+
+			&.mobile {
+				display: flex;
+			}
+		}
+	}
 </style>

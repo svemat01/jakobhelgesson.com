2a5471f908a90095fb674cc0ca66cc8dedcaa85c
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-02-27 22:17:44 +0000

    Add ronne page

---
+++ src/routes/ronne/+page.svelte
@@ -0,0 +1,26 @@
+<div class="wifi">
+	<h1>BUNet-PSK</h1>
+
+	<p>S0xrYTmIrInBEZCvxKCzspwY0nlJAPAv1ffg6aKF</p>
+</div>
+
+<style lang="scss">
+	.wifi {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+
+		margin-top: 4rem;
+	}
+
+	h1 {
+		color: $green;
+		font-size: 3.2rem;
+	}
+
+	p {
+		font-size: 1.8rem;
+	}
+</style>

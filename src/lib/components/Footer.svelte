21802825e283ddbdf308ea4707b36bdc75da8211
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-03-09 21:18:21 +0000

    Fix footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,58 +3,57 @@
 </script>
 
 <footer>
-        <div class="container">
-            <div class="logo">
-                <img src={labsIcon} alt="Helgesson Labs Logo">
-                <span class="title">Helgesson <span class="thin">Labs</span></span>
-            </div>
-            <!-- <div class="links">
+	<div class="container">
+		<div class="logo">
+			<img src={labsIcon} alt="Helgesson Labs Logo" />
+			<span class="title">Helgesson <span class="thin">Labs</span></span>
+		</div>
+		<!-- <div class="links">
                 <a href="/about">About</a>
                 <a href="/contact">Contact</a>
             </div> -->
-        </div>
+	</div>
 </footer>
 
 <style lang="scss">
-    footer {
-        background-color: $dark-gray;
-        width: 100%;
-        height: 13rem;
+	footer {
+		background-color: $gray-350;
+		width: 100%;
+		height: 13rem;
 
-        margin-top: auto;
+		margin-top: 10rem;
 
-        padding: 2rem 0;
-    }
+		padding: 2rem 0;
+	}
 
-    .container {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-rows: 1fr;
+	.container {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr;
 
-        max-width: 1200px;
-        width: 100%;
-        
-        margin: 0 auto;
-    }
+		max-width: 1200px;
+		width: 100%;
 
-    .logo {
+		margin: 0 auto;
+	}
 
-        display: flex;
-        align-items: center;
-        gap: 1.4rem;
+	.logo {
+		display: flex;
+		align-items: center;
+		gap: 1.4rem;
 
 		.title {
 			font-size: 2.4rem;
 			font-weight: 400;
-			color: $white;
+			color: $gray-900;
 
-            .thin {
-                font-weight: 200;
-            }
+			.thin {
+				font-weight: 200;
+			}
 		}
 
-        img {
-            height: 40px;
-        }
-    }
+		img {
+			height: 40px;
+		}
+	}
 </style>

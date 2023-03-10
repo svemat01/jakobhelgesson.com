86518fbbdad114e2aacbc22b8fd7328644234012
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-03-10 18:45:05 +0000

    Introduce diagonal comp

---
+++ src/lib/components/Diagonal.svelte
@@ -0,0 +1,30 @@
+<div class="section">
+	<div class="wrapper container">
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.section {
+		position: relative;
+		isolation: isolate;
+
+		margin-bottom: 4rem;
+
+		&::before {
+			content: '';
+			background-color: $gray-350;
+
+			position: absolute;
+			z-index: -1;
+			inset: 0;
+
+			margin-inline: auto;
+			transform: skewY(var(--skew-y, -2deg));
+		}
+
+		.wrapper {
+			padding-block: 12rem;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,18 @@
 <script>
+	import Diagonal from '$lib/components/Diagonal.svelte';
 	import BlogPosts from '$lib/sections/blog-posts/BlogPosts.svelte';
-import Hero from '$lib/sections/hero/Hero.svelte';
+	import Hero from '$lib/sections/hero/Hero.svelte';
 	import LatestWorks from '$lib/sections/latest-works/LatestWorks.svelte';
 	import Me from '$lib/sections/me/Me.svelte';
 </script>
 
 <Hero />
 
-<div class="section">
-	<div class="wrapper container">
-		<Me />
-	</div>
-</div>
+<Diagonal --skew-y="-2deg">
+	<Me />
+</Diagonal>
 
 <LatestWorks />
 
 <BlogPosts />
 
-<style lang="scss">
-	.section {
-		position: relative;
-		isolation: isolate;
-
-		margin-bottom: 4rem;
-
-		&::before {
-			content: '';
-			background-color: $gray-350;
-
-			position: absolute;
-			z-index: -1;
-			inset: 0;
-
-			margin-inline: auto;
-			transform: skewY(-2deg);
-		}
-
-		.wrapper {
-			padding-block: 12rem;
-		}
-	}
-</style>

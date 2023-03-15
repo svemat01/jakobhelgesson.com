a486985c03daaf3df29765984f7525ffdc9586b2
Jakob Helgesson <jakob.helgesson@gmail.com>
2023-03-15 20:45:58 +0000

    Remove comment

--- src/lib/sections/skills/Skill.svelte
+++ src/lib/sections/skills/Skill.svelte
@@ -11,9 +11,6 @@
 <style lang="scss">
     div {
         display: flex;
-        /* flex-direction: column;
-        align-items: center;
-        justify-content: center; */
         gap: 1.5rem;
     }
 
